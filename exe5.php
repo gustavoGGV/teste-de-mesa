16b9eed3810a7b70bd2f758df91c354480d90abb
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2023-11-30 22:25:02 -0300

    Create exe5.php

---
+++ exe5.php
@@ -0,0 +1,13 @@
+<?php
+
+$n = array(
+    array(1,3,5),
+    array(7,9,11),
+    array(13,15,17),
+);
+foreach ($n as $l) {
+    foreach ($l as $n) {
+    print $n . ' ';
+    }
+    print "\n";
+}
